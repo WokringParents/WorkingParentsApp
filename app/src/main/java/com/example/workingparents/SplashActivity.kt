1125873219351ed9a8a84c2f8ed39ab121c9600d
rodwnl <qurtks2224@gmail.com>
2022-08-05 01:40:54 +0900

    splash 추가 ui 수정

---
+++ app/src/main/java/com/example/workingparents/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.workingparents
+
+import android.animation.ObjectAnimator
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        progressBar.max=100
+
+        val currentProgress = 100
+
+        ObjectAnimator.ofInt(progressBar, "progress", currentProgress)
+            .setDuration(3000)
+            .start()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        },3000)
+
+    }
+}
