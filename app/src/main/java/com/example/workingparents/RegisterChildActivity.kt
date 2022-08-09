7a7784cd76788c6aa161d3f43af23953e82e7748
rodwnl <qurtks2224@gmail.com>
2022-08-09 17:00:23 +0900

    깃허브 확인용 푸시

---
+++ app/src/main/java/com/example/workingparents/RegisterChildActivity.kt
@@ -0,0 +1,11 @@
+package com.example.workingparents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterChildActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_child)
+    }
+}
