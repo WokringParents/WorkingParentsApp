21af8ac5605fd57c9741e5ed1e47c72226d0ccc5
지연 <88127852+codingGoover@users.noreply.github.com>
2022-08-14 02:58:39 +0900

    chidCareSharing : 현재 날짜와 적용가능한 UI로

--- app/src/main/java/com/example/workingparents/ChildCaringFragment.kt
+++ app/src/main/java/com/example/workingparents/ChildCaringFragment.kt
@@ -2,25 +2,38 @@ package com.example.workingparents
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.fragment_child_caring.*
 import java.text.SimpleDateFormat
 import java.util.*
-import java.time.DayOfWeek;
+import kotlin.properties.Delegates
 
 class ChildCaringFragment : Fragment() {
 
     private lateinit var mContext : Activity
     private lateinit var sharingListAdapter: SharingListAdapter
     val datas = mutableListOf<SharingList>()
+
+    /*UI초기화에 필요한 현재의 연도,달,일주일날짜,요일(월:0~일:6)*/
+    lateinit var year: String
+    lateinit var month: String
+    lateinit var day: String
+    var todayOfWeek by Delegates.notNull<Int>()
+    var dayArr: Array<String?> = arrayOfNulls<String>(7)
+
+    /*Retrofit서비스에 필요한 Timstamp*/
+
+
     private var TAG ="ChildCaring"
 
     override fun onAttach(context: Context) {
@@ -46,13 +59,15 @@ class ChildCaringFragment : Fragment() {
         val long_now= System.currentTimeMillis()
         val curDate= Date(long_now)
 
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale("ko", "KR"))
+        val dateFormat = SimpleDateFormat("yyyy-M-d", Locale("ko", "KR"))
         val str_date = dateFormat.format(curDate)
-        week(str_date)
 
-      //val t_dateFormat = SimpleDateFormat("yyyy-MM-dd kk:mm:ss E", Locale("ko", "KR"))
+        getCurrentWeek(str_date)
+        setDateUI(view)
 
-      //  week("2022-8-1")
+
+        //val t_dateFormat = SimpleDateFormat("yyyy-MM-dd kk:mm:ss E", Locale("ko", "KR"))
+
         initRecycler(recyclerView)
 
         return view
@@ -91,31 +106,103 @@ class ChildCaringFragment : Fragment() {
     }
 
 
-    fun week(eventDate: String) {
+    fun getCurrentWeek(eventDate: String) {
         val dateArray = eventDate.split("-").toTypedArray()
+        //연도-월-일
+        year = dateArray[0]
+        month = dateArray[1]
+        day=dateArray[2]
 
         val cal = Calendar.getInstance()
         cal[dateArray[0].toInt(), dateArray[1].toInt() - 1] = dateArray[2].toInt()
+        cal.firstDayOfWeek = Calendar.MONDAY   // 일주일의 첫날을 월요일로 지정한다
+        val dayOfWeek = cal[Calendar.DAY_OF_WEEK] - cal.firstDayOfWeek // 시작일과 특정날짜의 차이를 구한다
 
-        // 일주일의 첫날을 월요일로 지정한다
-        cal.firstDayOfWeek = Calendar.MONDAY
+        // 해당 주차의 첫째날을 지정한다  // 일요일일 때가 dayofmonth가 특수하게 -1로 떠서 그에 맞게 처리해줌
+        if (dayOfWeek == -1) {
+            cal.add(Calendar.DAY_OF_MONTH, -6)
+            todayOfWeek=6
+        } else {
+            cal.add(Calendar.DAY_OF_MONTH, -dayOfWeek)
+            todayOfWeek=dayOfWeek
+        }
 
-        // 시작일과 특정날짜의 차이를 구한다
-        val dayOfWeek = cal[Calendar.DAY_OF_WEEK] - cal.firstDayOfWeek
+        val dateformat = SimpleDateFormat("yyyy-MM-dd")
+        val dayformat = SimpleDateFormat("d")
 
-        // 해당 주차의 첫째날을 지정한다
-        cal.add(Calendar.DAY_OF_MONTH, -dayOfWeek)
+        var dayVal = dayformat.format(cal.time).toInt()
 
-        val sf = SimpleDateFormat("yyyy-MM-dd")
+        for (i in 0 until dayArr.count()){
+            dayArr[i]=dayVal.toString()
+            dayVal++
+        }
 
         // 해당 주차의 첫째 날짜
-        val startDt = sf.format(cal.time)
+        val startDt = dateformat.format(cal.time)
 
-        // 해당 주차의 마지막 날짜 지정
+         // 해당 주차의 마지막 날짜 지정
         cal.add(Calendar.DAY_OF_MONTH, 6)
 
         // 해당 주차의 마지막 날짜
-        val endDt = sf.format(cal.time)
+        val endDt = dateformat.format(cal.time)
         Log.d(TAG, "특정 날짜 = [$eventDate] >> 시작 날짜 = [$startDt], 종료 날짜 = [$endDt]")
+
+
+    }
+
+
+    fun setDateUI(view: View){
+
+         //날짜 dd
+        val dayNumTV :Array<TextView> = arrayOf(
+            view.findViewById<TextView>(R.id.monDateTV),
+            view.findViewById<TextView>(R.id.tueDateTV),
+            view.findViewById<TextView>(R.id.wenDateTV),
+            view.findViewById<TextView>(R.id.thuDateTV),
+            view.findViewById<TextView>(R.id.friDateTV),
+            view.findViewById<TextView>(R.id.satDateTV),
+            view.findViewById<TextView>(R.id.sunDateTV)
+         )
+
+        //월,화,수,,한글
+        val dayHangulTV :Array<TextView> = arrayOf(
+            view.findViewById<TextView>(R.id.monTextView),
+            view.findViewById<TextView>(R.id.tueTextView),
+            view.findViewById<TextView>(R.id.wenTextView),
+            view.findViewById<TextView>(R.id.thuTextView),
+            view.findViewById<TextView>(R.id.friTextView),
+            view.findViewById<TextView>(R.id.satTextView),
+            view.findViewById<TextView>(R.id.sunTextView),
+        )
+
+        //월화수,,이미지버튼
+        val dayImgBtn: Array<ImageButton> = arrayOf(
+            view.findViewById<ImageButton>(R.id.monBtn),
+            view.findViewById<ImageButton>(R.id.tueBtn),
+            view.findViewById<ImageButton>(R.id.wenBtn),
+            view.findViewById<ImageButton>(R.id.thuBtn),
+            view.findViewById<ImageButton>(R.id.friBtn),
+            view.findViewById<ImageButton>(R.id.satBtn),
+            view.findViewById<ImageButton>(R.id.sunBtn)
+           )
+
+         view.findViewById<TextView>(R.id.yearTextView).text=year
+         view.findViewById<TextView>(R.id.monthTextView).text=month
+
+        for(i: Int in 0..6){
+            dayNumTV[i].text= dayArr[i]
+            if(i==todayOfWeek){
+                //일단 테스트한다고 오늘이면 글자두껍고 색깔블랙으로 설정함 언제쯤 디자인 넘어올까 ㅜㅡㅜ
+                dayNumTV[i].setTypeface(dayNumTV[i].typeface, Typeface.BOLD)
+                dayNumTV[i].setTextColor(Color.BLACK)
+                dayHangulTV[i].setTypeface(dayHangulTV[i].typeface, Typeface.BOLD)
+                dayHangulTV[i].setTextColor(Color.BLACK)
+                dayImgBtn[i].setImageResource(R.drawable.fullheart)
+            }
+
+        }
+
+
+
     }
 }
