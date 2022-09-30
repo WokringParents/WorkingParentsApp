54d9f9b4cfc1569203c4a38eb8e99deadc374c76
eunalove <88129325+eunalove@users.noreply.github.com>
2022-10-01 04:34:16 +0900

    캘린더 프레그먼트 전환

---
+++ app/src/main/java/com/example/workingparents/CalendarFragment.kt
@@ -0,0 +1,44 @@
+package com.example.workingparents
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CalendarFragment : Fragment() {
+
+    companion object{
+        const val TAG: String = "로그"
+
+        fun newInstance(): CalendarFragment{
+            return CalendarFragment()
+        }
+    }
+
+    //메모리에 올라갔을 때
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Home- onCreate")
+    }
+
+    //프레그먼트를 안고 있는 액티비티에 붙었을 때
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "Home- onAttach")
+    }
+
+    //뷰가 생성되었을 때
+    //프레그먼트와 레이아웃을 연결시켜주는 부분
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG, "Home- onAttach")
+        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
+
+}
